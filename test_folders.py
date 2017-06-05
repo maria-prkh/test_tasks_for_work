7a33ed62f32cc1a9fb219e70114aba3bf4e73b81
Maria <marykostenko@gmail.com>
2017-06-05 23:16:25 +0200

    extended client to support different types of API (fs, fs-content) added test_upload_file

--- test_folders.py
+++ test_folders.py
@@ -2,23 +2,35 @@ import json
 import unittest
 
 import requests
+import tempfile
+
+
+API_TYPE_FS = 'fs'
+API_TYPE_FS_CONTENT = 'fs-content'
 
 
 class EgnyteClient(object):
     host_name = 'newparity.qa-egnyte.com'
     token = 'bjy9hd796yx7gxpeju5tq59t'
 
-    def get_url(self, folder_path):
+    def get_url(self, full_path, api_type):
         """Gets url for a concrete folder."""
+        supported_api_types = ('fs', 'fs-content')
+        if api_type not in supported_api_types:
+            raise ValueError('{} API type is not supported'.format(api_type))
 
-        url_tpl = 'https://{host}/pubapi/v1/fs/{folder}'
-        return url_tpl.format(host=self.host_name, folder=folder_path)
+        url_tpl = 'https://{host}/pubapi/v1/{api_type}/{full_path}'
+        return url_tpl.format(
+            host=self.host_name,
+            full_path=full_path,
+            api_type=api_type
+        )
 
     def get_listing(self, folder_name):
         """To get json with list of folders which contains specified folder."""
 
         headers = self.get_headers()
-        url = self.get_url(folder_name)
+        url = self.get_url(folder_name, API_TYPE_FS)
         resp = requests.get(url, headers=headers)
 
         resp.raise_for_status()
@@ -31,7 +43,7 @@ class EgnyteClient(object):
         headers = self.get_headers()
 
         data = {"action": "add_folder"}
-        url = self.get_url(folder_path)
+        url = self.get_url(folder_path, API_TYPE_FS)
 
         resp = requests.post(url, data=json.dumps(data), headers=headers)
         resp.raise_for_status()
@@ -39,7 +51,7 @@ class EgnyteClient(object):
     def remove_folder(self, folder_path, ignore_not_found=False):
         headers = self.get_headers()
 
-        url = self.get_url(folder_path)
+        url = self.get_url(folder_path, API_TYPE_FS)
 
         resp = requests.delete(url, headers=headers)
 
@@ -56,9 +68,14 @@ class EgnyteClient(object):
         }
         return headers
 
-    def upload_file(self, folder_path, file_path):
+    def upload_file(self, fp, file_path):
         """Uploads a file to the specified folder path."""
-        pass
+        headers = self.get_headers()
+        url = self.get_url(file_path, API_TYPE_FS_CONTENT)
+
+        resp = requests.post(url, headers=headers, files={'file': fp})
+
+        resp.raise_for_status()
 
     def rename_folder(self, original_path, destination_path):
         headers = self.get_headers()
@@ -68,7 +85,7 @@ class EgnyteClient(object):
             "destination": destination_path
         }
 
-        url = self.get_url(original_path)
+        url = self.get_url(original_path, API_TYPE_FS)
         response = requests.post(url, data=json.dumps(data), headers=headers)
         response.raise_for_status()
 
@@ -156,6 +173,14 @@ class TestFilesAndFoldersListing(unittest.TestCase):
                                    folder_listing['folders']]
         self.assertNotIn(new_folder, folder_names_in_listing)
 
+    def test_upload_file(self):
+        parent_folder = 'Shared/__Maria'
+        file_name = 'TestUploadFile.txt'
+        full_path = '{}/{}'.format(parent_folder, file_name)
 
+        with tempfile.TemporaryFile() as fp:
+            fp.write(b'Hello World!\n')
+            fp.seek(0)
 
-
+            # TODO: add cleanup for created flder with file
+            self.client.upload_file(fp, full_path)
